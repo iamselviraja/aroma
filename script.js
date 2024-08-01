4d169eefe0ae7662878f9c9c7a13ccb5153d8ac3
Selviraja <i.am.selviraja.p@gmail.com>
2024-08-01 09:44:04 +0530

    Exclusives section has been completed

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const btnToggleBar2 = document.querySelector(".toggle-btn-bar-2");
 const btnToggleBar3 = document.querySelector(".toggle-btn-bar-3");
 const navMenuEl = document.querySelector(".header__nav--menu");
 
+const exclusivesTabs = document.querySelectorAll(".exclusives__tabs-tab");
+const exclusivesTabsContents = document.querySelectorAll(".exclusives__desc--tab");
+
+/**
+ * Function to toggle navigation menu
+ */
 const toggleNavMenu = () => {
     // Animate nav-toggle button
     btnToggleBar1.classList.toggle('active');
@@ -14,5 +20,36 @@ const toggleNavMenu = () => {
     navMenuEl.classList.toggle("active");
 }
 
+btnToggle.addEventListener('click', () =>  toggleNavMenu());
 
-btnToggle.addEventListener('click', () =>  toggleNavMenu() );
+/**
+ * Function to switch tabs
+ */
+const switchTabs = (activeTabId,  activeTabContentId) => {
+    // Remove active styles on all tab
+    exclusivesTabs.forEach(tab => {
+        tab.classList.remove("exclusives__tabs--active");
+    });
+
+    // Use activeTabId to apply active styles to the clicked tab
+    document.getElementById(activeTabId).classList.add("exclusives__tabs--active");
+
+    // Remove active styles on all tab contents
+    exclusivesTabsContents.forEach(tab => {
+        tab.classList.remove('exclusives__desc--active-tab');
+    });
+
+    // Use activeTabContentId to apply active styles to the clicked tab content
+    document.getElementById(activeTabContentId).classList.add("exclusives__desc--active-tab");
+};
+
+exclusivesTabs.forEach(tab => tab.addEventListener("click", () => {
+    // Extract the tab's data value
+    const activeTabId = `exclusives__tabs--${tab.getAttribute('data-tab')}`;
+
+    // Construct the active tab content's id
+    const activeTabContentId = `exclusives__desc--tab-${tab.getAttribute('data-tab')}`;
+
+    // Switch tabs to extracted tab id
+    switchTabs(activeTabId, activeTabContentId);
+}));
